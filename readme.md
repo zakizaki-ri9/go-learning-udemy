3b5c93d303fe1d1e949937d8c1b895acbadf2a52
zakizaki-ri9 <kic.yuuki@gmail.com>
2018-11-25 14:00:41 +0900

    struct setter method

--- readme.md
+++ readme.md
@@ -14,8 +14,14 @@ func (v Vertex) Area() int {
 	return v.X * v.Y
 }
 
+func (v *Vertex) Scale(i int) {
+	v.X = v.X * i
+	v.Y = v.Y * i
+}
+
 func main() {
-	v := Vertex{3, 4}
+  v := Vertex{3, 4}
+  v.Scale(100)
 	fmt.Println(v.Area())
 }
 ```
--- section6/01-pointer.go
+++ section6/01-pointer.go
@@ -12,6 +12,11 @@ func (v Vertex) Area() int {
 	return v.X * v.Y
 }
 
+func (v *Vertex) Scale(i int) {
+	v.X = v.X * i
+	v.Y = v.Y * i
+}
+
 func Area(v Vertex) int {
 	return v.X * v.Y
 }
@@ -19,5 +24,6 @@ func Area(v Vertex) int {
 func main() {
 	v := Vertex{3, 4}
 	fmt.Println(Area(v))
+	v.Scale(10)
 	fmt.Println(v.Area())
 }
