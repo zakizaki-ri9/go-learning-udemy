093e04dcc08a2ae90fef1fa27399f8d671bbfa20
zakizaki-ri9 <kic.yuuki@gmail.com>
2018-11-25 20:59:05 +0900

    embedded

---
+++ section6/sample01.go
@@ -0,0 +1,44 @@
+package main
+
+import "fmt"
+
+type Vertex struct {
+	x, y int
+}
+
+func (v Vertex) Area() int {
+	return v.x * v.y
+}
+
+func (v *Vertex) Scale(i int) {
+	v.x = v.x * i
+	v.y = v.y * i
+}
+
+type Vertex3D struct {
+	Vertex
+	z int
+}
+
+func (v Vertex3D) Area3D() int {
+	return v.x * v.y * v.z
+}
+
+func (v *Vertex3D) Scale3D(i int) {
+	v.x = v.x * i
+	v.y = v.y * i
+	v.z = v.z * i
+}
+
+func New(x, y, z int) *Vertex3D {
+	return &Vertex3D{Vertex{x, y}, z}
+}
+
+func main() {
+	//v := Vertex{3, 4}
+	v := New(3, 4, 5)
+	//fmt.Println(Area(v))
+	v.Scale3D(10)
+	fmt.Println(v.Area())
+	fmt.Println(v.Area3D())
+}
